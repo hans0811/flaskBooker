1ee63eced1c313f7b67e7c1821a0a1b63692866a
hans <hans0811>
2022-10-09 23:10:39 +0800

    refactor booker to ooj

--- app/view_models/book.py
+++ app/view_models/book.py
@@ -1,5 +1,31 @@
 
 
+class BookViewModel:
+    def __init__(self, book):
+        self.title = book['title']
+        self.publisher = book['publisher']
+        self.author = '、'.join(book['author'])
+        self.image = book['image']
+        self.price = book['price']
+        self.summary = book['summary']
+        self.isbn = book['isbn']
+        self.pages = book['pages']
+        self.pubdate = book['pubdate']
+        self.binding = book['binding']
+
+
+class BookCollection:
+    def __init__(self):
+        self.total = 0
+        self.books = []
+        self.keyword = ''
+
+    def fill(self, booker_book, keyword):
+        self.total = booker_book.total
+        self.keyword = keyword
+        self.books = [BookViewModel(book) for book in booker_book.books]
+
+
 # Not use
 class _BookViewModel:
     @classmethod
--- app/web/book.py
+++ app/web/book.py
@@ -4,7 +4,7 @@ from app.spider.booker_book import BookerBook
 from app.libs.helper import is_isbn_or_key
 from . import web
 from app.forms.book import SearchForm
-from ..view_models.book import BookViewModel
+from ..view_models.book import BookViewModel, BookCollection
 
 
 @web.route('/test')
@@ -22,25 +22,29 @@ def test1():
 @web.route('/book/search')
 def search():
     # app.add_url_rule
-
     # # At least 1 character
     # q = request.args['q']
     # # length limit
     # page = request.args['page']
 
     form = SearchForm(request.args)
+    books = BookCollection()
+
     # validation layer
     if form.validate():
         q = form.q.data.strip() # delete space
         page = form.page.data
         isbn_or_key = is_isbn_or_key(q)
+        booker_book = BookerBook()
+
         if isbn_or_key == 'isbn':
-            result = BookerBook.search_by_isbn(q)
-            result = BookViewModel.package_single(result, q)
+            booker_book.search_by_isbn(q)
         else:
-            result = BookerBook.search_by_keyword(q, page)
-            result = BookViewModel.package_collection(result, q)
-        return jsonify(result)
+            booker_book.search_by_keyword(q, page)
+
+        #__dict__
+        books.fill(booker_book, q)
+        return jsonify(books)
     else:
         return jsonify(form.errors)
 
